187e4b3a8df546fb26b131d31299f1d10c296b57
Olga <glazkovaolga_78@mail.ru>
2024-08-11 01:54:46 +0500

    Add files via upload

---
+++ task_module_1.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students1 = sorted(students)
+(sum(grades[0]) / len(grades[0]))
+(sum(grades[1]) / len(grades[1]))
+(sum(grades[2]) / len(grades[2]))
+(sum(grades[3]) / len(grades[3]))
+(sum(grades[4]) / len(grades[4]))
+grades1 = [(sum(grades[0]) / len(grades[0])), (sum(grades[1]) / len(grades[1])), (sum(grades[2]) / len(grades[2])), (sum(grades[3]) / len(grades[3])) ,(sum(grades[4]) / len(grades[4]))]
+wordbook = dict(zip(students1, grades1))
+print(wordbook)
